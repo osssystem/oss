1e375dde3295f7ef0a088b352f8f0bdee9b70f4c
Vinkourov Alexander <alex@snowworld.ru>
2016-04-29 20:57:05 +0300

    2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# oss
+# Open source search
+
+Team 2 educational tceh project. Main purpose is to connect open source project maintainers and open source developers to each other with user friendly interface,
--- oss_main/views.py
+++ oss_main/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 def index(request):
-    return HttpResponse("Welcome to OSS!")
+    if request.method == 'GET':
+        return render_to_response('oss_main/index.html')
+    return HttpResponse(status=405)
