fb87e4dd0b06bdb0fa60fa9f8e058efedf217211
Kirill Savelyev <kirillsavelyev@yandex.ru>
2016-04-29 21:10:43 +0300

    models

--- oss_main/models.py
+++ oss_main/models.py
@@ -2,4 +2,53 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField()
+    surname = models.CharField()
+    email = models.EmailField()
+    password = models.PasswordField()
+    login_name = models.CharField()
+
+
+class UserSkill(models.Model):
+    user = models.ForeignKey('User')
+    skill = models.ForeignKey('Skill')
+    level = models.ForeignKey('Level')
+
+
+class Project(models.Model):
+    name = models.CharField()
+    url = models.CharField()
+
+
+class ProjectOwner(models.Model):
+    project = models.ForeignKey('Project')
+    owner = models.ForeignKey('User')
+
+
+class Issue(models.Model):
+    project = models.ForeignKey('Project')
+    name = models.CharField()
+    author = models.ForeignKey('User')
+    url = models.CharField() # or
+    # text = models.CharField() # or
+
+
+class IssueSkill(models.Model):
+    issue = models.ForeignKey('Issue')
+    skill = models.ForeignKey('Skill')
+    level = models.ForeignKey('Level')
+
+
+class IssueDeveloper(models.Model):
+    issue = models.ForeignKey('Issue')
+    developer = models.ForeignKey('User')
+
+
+class Skill(models.Model):
+    name = models.CharField()
+
+class Level(models.Model):
+    name = models.CharField()
+
 
