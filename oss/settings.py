d59bc6ffe0370bb6edf74cfc90e19b224d648093
Andrey <andy_evl@mail.ru>
2016-05-20 21:19:31 +0300

    created staticfolder 2

--- oss/settings.py
+++ oss/settings.py
@@ -130,13 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = './static/'
 
 STATIC_ROOT = './static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'bower_components'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'bower_components'),
+#]
 
 AUTH_USER_MODEL = 'oss_main.User'
 LOGIN_URL = 'oss_main:index'
