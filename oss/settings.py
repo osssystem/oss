af5fb10a8441347e1059441eb4271cc3f12b84f1
Andrey <andy_evl@mail.ru>
2016-05-21 10:07:44 +0300

    added whitenoise for static files, Debug mode returned

--- oss/settings.py
+++ oss/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd0at*m2!c3cx7qundwo&=x!vipnj#39hv096m2=q@lls#4on%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['open-source-search.herokuapp.com']
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['open-source-search.herokuapp.com']
 
 
 # Application definition
@@ -51,6 +52,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'oss.urls'
@@ -126,17 +128,21 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = './static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = './static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'bower_components'),
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'bower_components'),
+]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'oss_main.User'
 LOGIN_URL = 'oss_main:index'
